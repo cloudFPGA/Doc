6f2c3137bc872aaa04f86db978fc5e4f1a7f30ce
Dionysios Diamantopoulos @ vm on laptop <did@zurich.ibm.com>
2020-12-16 11:57:52 +0100

    Rolled back to fixed window width, not proportional with custom css.

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -117,11 +117,11 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static', '../notes']
-html_extra_path = ['../notes']
+#html_extra_path = ['../notes']
 
-html_css_files = [
-    'custom.css',
-]
+#html_css_files = [
+#    'custom.css',
+#]
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
